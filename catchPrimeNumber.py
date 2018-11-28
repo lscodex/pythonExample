ffb615559e71053e70662563e0289be50827353c
lscodex <lscodexx@gmail.com>
2018-11-28 15:19:19 +0300

    add program

---
+++ catchPrimeNumber.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+def isPrimeNumber(primeNum):
+	if primeNum > 1:   #  must greater than one
+		if(primeNum==2):
+			print(primeNum,"This number is not prime number!")
+			return False
+		for i in range(2,primeNum):   # a range of entered value
+			if (primeNum % i) == 0: # equl to zero 
+				print(primeNum,"this number is not prime number!")
+				return False
+			else:
+				print(primeNum,"Yep, it is prime number")
+				return True
+	else:
+		print(primeNum," Sorry that but you entered 1! ")
+		return True
+
+def catchPrimeNumber():
+	primeNum = int(raw_input("Please, enter to number: \n"))  # waiting user to enter number
+	isPrimeNumber(primeNum) # called function
+	for i in range(2,primeNum + 1): # show the prime numbers up to the user's numbers.
+		for j in  range (2,i):
+			if(i % j) == 0:
+				break
+ 			else:
+				print(i)
+				break
+
+catchPrimeNumber()
+
